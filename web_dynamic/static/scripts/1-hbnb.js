315f3fe78f81c62ed365b8b98df2350881d7d282
Karim-Moharm <karimmoharm222@gmail.com>
2023-11-03 18:02:18 +0200

    push

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    let amenities = [];
+    let checkboxId = $('#checkbox-id');
+
+    if (checkboxId.checked) {
+        console.log('done');
+    }
+    // $('input').change(() => {
+    //     if (checkboxId.checked) {
+    //         console.log('done');
+    //     }
+    // });
+});
