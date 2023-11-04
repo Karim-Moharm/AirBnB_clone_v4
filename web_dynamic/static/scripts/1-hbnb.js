6cabac9f910a780dea83bf3605c592e64d2c9a7d
Karim-Moharm <karimmoharm222@gmail.com>
2023-11-04 17:32:52 +0200

    testing checkbox

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,13 +1,41 @@
-$(document).ready(function () {
-    let amenities = [];
-    let checkboxId = $('#checkbox-id');
 
-    if (checkboxId.checked) {
-        console.log('done');
-    }
-    // $('input').change(() => {
-    //     if (checkboxId.checked) {
-    //         console.log('done');
-    //     }
-    // });
+// document.addEventListener("DOMContentLoaded", () => {
+//     $(document).ready(function () {
+//         let amenities = [];
+//         let checkboxId = $('#checkbox-id');
+//         let temp = undefined;
+//         $('input').on('change', () => {
+//             if (checkboxId.is(':checked')) {
+//                 // console.log('done');
+//                 temp = checkboxId.attr('data-id');
+//                 console.log(temp);
+//                 amenities.push(temp);
+//             }
+//             else {
+//                 delete checkboxId.attr('data-id');
+//             }
+//         });
+//     });
+// });
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    $(document).ready(function () {
+        const selectedAmenities = {}; // Use an object to store the selected amenities
+
+        $('input[type="checkbox"]').on('change', function () {
+            const amenityId = $(this).data('id');
+            const amenityName = $(this).data('name');
+
+            if (this.checked) {
+                selectedAmenities[amenityId] = amenityName; // Add to the selected amenities
+            } else {
+                delete selectedAmenities[amenityId]; // Remove from the selected amenities
+            }
+
+            // Update the h4 tag with the list of selected amenities
+            const h4Tag = $('.amenities h4');
+            h4Tag.text(Object.values(selectedAmenities).join(', '));
+        });
+    });
 });
